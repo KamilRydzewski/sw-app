d414cbf4dae9c09879dea8bfe59a459e37550710
kamil <krydzewski@unitdoseone.com>
2021-02-03 19:12:54 +0100

    NavMenu test

--- src/components/commonUI/Navigation/MainNavigation.test.tsx
+++ src/components/commonUI/Navigation/MainNavigation.test.tsx
@@ -4,6 +4,7 @@ import "jest-styled-components";
 import { ThemeProvider } from "styled-components";
 import { theme } from "src/theme/mainTheme";
 import { HashRouter } from "react-router-dom";
+import {NavigationSchema} from './NavigationSchema';
 
 describe("MainNavigation component", () => {
   it("renders MainNavigation element", () => {
@@ -16,5 +17,18 @@ describe("MainNavigation component", () => {
         </ThemeProvider>
     );
     expect(getByTestId("main-navigation")).toBeInTheDocument();
+  });
+  it("renders MainNavigation routes list", () => {
+      const {getByText} = render(
+          <ThemeProvider theme={theme}>
+              <HashRouter>
+              <MainNavigation/>
+              </HashRouter>
+          </ThemeProvider>
+      );
+     NavigationSchema.map( route => route.name).forEach(routeName=>{
+        expect(getByText(routeName)).toBeInTheDocument();
+    })
+   
   });
 });
